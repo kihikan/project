6e7f270b224842461a258df1e3bb3e08ac97b036
Kirill <kihikan@gmail.com>
2021-09-20 10:02:53 +0300

    zadacha na switch

--- jvscr.js
+++ jvscr.js
@@ -1,2 +1,17 @@
-for (var i = 0; i< 10; i++);
-console.log(i);
+var num = 3;
+var result = ["zima","vesna", "osen", "leto"];
+switch (num)
+{
+    case 1:
+        alert(result[0]);
+    break;
+    case 2:
+        alert(result[1]);
+    break;
+    case 3:
+        alert(result[2]);
+    break;
+    case 4:
+        alert(result[3]);
+    break;
+}
