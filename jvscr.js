c7048e714eaf70fca6ac1c9354fa5e1e99ff3421
Kirill <kihikan@gmail.com>
2021-09-21 15:38:26 +0300

    zadanie na uslovie i cicly pravilnie

--- jvscr.js
+++ jvscr.js
@@ -13,35 +13,38 @@ const personalMovieDB = {
 
 for (let i = 0; i < 2; i++)
 {   
-    const lastFilm = prompt("Odin iz poslednih prosmotrennyh filmov ?", "");
+    const lastFilm = prompt("Odin iz poslednih prosmotrennyh filmov ?", ""),
+          lastFilmOcenka = prompt("Vo skolko ocenite ego ?","");
 
-    if(!lastFilm || lastFilm.length >50)
+   
+    if(lastFilm != null && lastFilmOcenka != null && lastFilm != "" && lastFilmOcenka != "" && lastFilm.length < 50)
+    {
+        personalMovieDB.movies[lastFilm] = lastFilmOcenka;
+    }
+    else
     {
         i--;
-    }else if (lastFilm.length > 0)
-    {
-        const lastFilmOcenka = prompt("Vo skolko ocenite ego ?","");
-        if(!lastFilmOcenka || lastFilmOcenka.length >50)
-        {
-            i--;
-        }else if(lastFilmOcenka) {personalMovieDB.movies[lastFilm] = lastFilmOcenka;}
     }
+    
 }
 
 
-if (personalMovieDB.count <10)
+
+
+if (personalMovieDB.count < 10)
 {
-    alert("Malo filmov smotrite");
     console.log("Malo filmov smotrite");
 }
-else if (personalMovieDB.count >=10 && personalMovieDB.count <=30)
+else if (personalMovieDB.count >=10 && personalMovieDB.count <30)
 {
-    alert("Vi klassichesky zritel");
+    console.log("Vi klassichesky zritel");
+} 
+else if (personalMovieDB.count >=30)
+{
     console.log("Vi klassichesky zritel");
 } 
 else 
 {
-    alert("Oshibka");
     console.log("Oshibka");
 }
 
