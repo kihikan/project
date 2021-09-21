7f4b312724d78e7f78446a3a628e896b5fefb423
Kirill <kihikan@gmail.com>
2021-09-21 10:23:57 +0300

    Zadanie 1

--- jvscr.js
+++ jvscr.js
@@ -1,17 +1,6 @@
-var num = 3;
-var result = ["zima","vesna", "osen", "leto"];
-switch (num)
-{
-    case 1:
-        alert(result[0]);
-    break;
-    case 2:
-        alert(result[1]);
-    break;
-    case 3:
-        alert(result[2]);
-    break;
-    case 4:
-        alert(result[3]);
-    break;
-}
+
+let numberOfFilms = prompt("Skolko filmov vi uge posmotreli ?");
+console.log(numberOfFilms);
+
+
+
