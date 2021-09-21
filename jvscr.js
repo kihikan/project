d8ccea544b2b49e29fffe5b8f015e6de4b9bdeea
Kirill <kihikan@gmail.com>
2021-09-21 10:41:08 +0300

    zadanie 2

--- jvscr.js
+++ jvscr.js
@@ -1,6 +1,20 @@
 
+//zadanie 1
 let numberOfFilms = prompt("Skolko filmov vi uge posmotreli ?");
-console.log(numberOfFilms);
+//console.log(numberOfFilms);
+///////////////////////////////
+
+//zadanie2
+let personalMovieDB = {
+    count: numberOfFilms, 
+    movies: {}, 
+    actors: {}, 
+    genres: [], 
+    privat: false
+};
+/////////////////////////////
+
+
 
 
 
