657f7380802715711cab09ca8be5e3cffe0ea284
Kirill <kihikan@gmail.com>
2021-09-23 13:18:08 +0300

    obiekty i desktr obiektov likcia

--- jvscr.js
+++ jvscr.js
@@ -1,94 +1,63 @@
 "use strict";
 
-let numberOfFilms;
+// function first(){
+//     //do something
+//     setTimeout(function(){
+//         console.log(1);
+//     }, 500);
+// }
 
+// function second(){
+//     console.log(2);
+// }
 
-function start()
-{
-    numberOfFilms = prompt("Skolko filmov vi uge posmotreli ?", "");
+// first();
+// second();
 
-    while (numberOfFilms =="" || numberOfFilms==null || isNaN(numberOfFilms))
-    {
-        numberOfFilms = prompt("Skolko filmov vi uge posmotreli ?", "");
+// function learnJS(lang, callback)
+// {
+//     console.log(`я учу: ${lang}`);
+//     callback();
+// }
+
+// function done(){
+//     console.log("я прошёл этот урок");
+// }
+
+// learnJS("JavaScript", done);
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: "black",
+        bg: "red"
+    }, 
+    makeTest: function(){
+        console.log("Test");
     }
-}
-
-start();
-
-
-const personalMovieDB = {
-    count: numberOfFilms, 
-    movies: {}, 
-    actors: {}, 
-    genres: [], 
-    privat: false
 };
 
+// delete options.name;
 
-function rememberMyFilms()
-{
-    
-    for (let i = 0; i < 2; i++)
-    {   
-    const lastFilm = prompt("Odin iz poslednih prosmotrennyh filmov ?", ""),
-          lastFilmOcenka = prompt("Vo skolko ocenite ego ?","");
+// console.log(options);
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof(options[key]) === "object"){
+//         for (let i in options[key]){
+//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+//         }
+//     }else{
+//     console.log(`Свойства ${key} имеет значение ${options[key]}`);
+//     }
+//     counter++;
+// }
+// console.log(counter);
 
-     if(lastFilm != null && lastFilmOcenka != null && lastFilm != "" && lastFilmOcenka != "" && lastFilm.length < 50)
-        {
-          personalMovieDB.movies[lastFilm] = lastFilmOcenka;
-        }
-    else
-        {
-         i--;
-        }
-    }
-}
+console.log(Object.keys(options).length);
+options.makeTest();
 
-rememberMyFilms();
+const{border, bg} = options.colors;
 
-
-function detectPersonalLevel()
-{
-    if (personalMovieDB.count < 10)
-    {
-        console.log("Malo filmov smotrite");
-    }
-    else if (personalMovieDB.count >=10 && personalMovieDB.count <30)
-    {
-        console.log("Vi klassichesky zritel");
-    } 
-    else if (personalMovieDB.count >=30)
-    {
-        console.log("Vi klassichesky zritel");
-    } 
-    else 
-    {
-        console.log("Oshibka");
-    }
-}
-
-detectPersonalLevel();
-
-function showMyDB(hidden)
-{ 
-    if (!hidden)
-    {
-        console.log(personalMovieDB);
-    } 
-}
-
-showMyDB(personalMovieDB.privat);
-
-function writeYouGenres()
-{
-    
-    
-    for (let i=1; i <= 3; i++)
-    {
-       personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`,"" );
-    }
-}
-
-writeYouGenres();
-
-//console.log(personalMovieDB);
+console.log(border);
