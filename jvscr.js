00e7ec680ee7995dad05385b3b7136c57acdfa6d
Kirill <kihikan@gmail.com>
2021-09-23 15:09:48 +0300

    lekcia massivy

--- jvscr.js
+++ jvscr.js
@@ -1,63 +1,43 @@
 "use strict";
 
-// function first(){
-//     //do something
-//     setTimeout(function(){
-//         console.log(1);
-//     }, 500);
-// }
+const arr = [2,3,6,25,8,10];
 
-// function second(){
-//     console.log(2);
-// }
+arr.sort(compareNum);
+console.log(arr);
 
-// first();
-// second();
+function compareNum(a, b) {
+    return a - b;
+}
 
-// function learnJS(lang, callback)
+
+// arr[99] = 0;
+
+// console.log(arr.length);
+// console.log(arr);
+
+// arr.forEach(function(item, i, arr) {
+//     console.log(`индекс ${i}: цифра ${item} внутри массива ${arr}`);
+
+// });
+
+// const str = prompt("","");
+// const products = str.split(", ");
+// console.log(products);
+
+//console.log(products.join("; "));
+
+
+// arr.pop();
+// console.log(arr);
+
+// arr.push(9);
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++)
 // {
-//     console.log(`я учу: ${lang}`);
-//     callback();
+//     console.log(arr[i]);
 // }
 
-// function done(){
-//     console.log("я прошёл этот урок");
+// for (let value of arr){
+//     console.log(value);
 // }
-
-// learnJS("JavaScript", done);
-
-const options = {
-    name: 'test',
-    width: 1024,
-    height: 1024,
-    colors: {
-        border: "black",
-        bg: "red"
-    }, 
-    makeTest: function(){
-        console.log("Test");
-    }
-};
-
-// delete options.name;
-
-// console.log(options);
-// let counter = 0;
-// for (let key in options) {
-//     if(typeof(options[key]) === "object"){
-//         for (let i in options[key]){
-//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
-//         }
-//     }else{
-//     console.log(`Свойства ${key} имеет значение ${options[key]}`);
-//     }
-//     counter++;
-// }
-// console.log(counter);
-
-console.log(Object.keys(options).length);
-options.makeTest();
-
-const{border, bg} = options.colors;
-
-console.log(border);
