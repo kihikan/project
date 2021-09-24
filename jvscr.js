9ee0dcd5fea6496ca6be77668e68acba71c3e0a2
Kirill <kihikan@gmail.com>
2021-09-24 09:19:18 +0300

    peredacha po ssilke ili po znacheniu, kopirovat massivy, operator spread

--- jvscr.js
+++ jvscr.js
@@ -1,43 +1,79 @@
 "use strict";
 
-const arr = [2,3,6,25,8,10];
+let a = 5,
+    b = a;
 
-arr.sort(compareNum);
-console.log(arr);
+b = b + 5;
 
-function compareNum(a, b) {
-    return a - b;
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; //Ссылка
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copyFunc(mainObj)
+{
+    let objCopy = {};
+    let key;
+
+    for (key in mainObj){
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
 }
 
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-// arr[99] = 0;
-
-// console.log(arr.length);
-// console.log(arr);
-
-// arr.forEach(function(item, i, arr) {
-//     console.log(`индекс ${i}: цифра ${item} внутри массива ${arr}`);
-
-// });
-
-// const str = prompt("","");
-// const products = str.split(", ");
-// console.log(products);
-
-//console.log(products.join("; "));
+// const newNumbers = copyFunc(numbers);
+// newNumbers.a = 10;
+// newNumbers.c.x = 99;
 
 
-// arr.pop();
-// console.log(arr);
+// console.log(numbers);
+// console.log(newNumbers);
 
-// arr.push(9);
-// console.log(arr);
+const add = {
+    d: 17,
+    e: 20
+};
 
-// for (let i = 0; i < arr.length; i++)
-// {
-//     console.log(arr[i]);
-// }
+//console.log(Object.assign(numbers, add));
 
-// for (let value of arr){
-//     console.log(value);
-// }
+const clone = Object.assign({}, add);
+clone.d = 99;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ["a","b","c"];
+
+const newArray = oldArray.slice();
+
+newArray[1] = "blablabal";
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ["youtube", "vimeo", "rutube"],
+      blogs = ["wordpress", "livejournal", "blogger"],
+      internet = [...video, ...blogs, "vk", "facebook"];
+ 
+console.log(internet);
+
