e4aa99133ddd18696d90b324acc209d3af40c571
Kirill <kihikan@gmail.com>
2021-09-24 11:11:26 +0300

    lekcia oop

--- jvscr.js
+++ jvscr.js
@@ -1,79 +1,22 @@
-"use strict";
+//"use strict";
 
-let a = 5,
-    b = a;
-
-b = b + 5;
-
-console.log(b);
-console.log(a);
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-// const copy = obj; //Ссылка
-
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copyFunc(mainObj)
-{
-    let objCopy = {};
-    let key;
-
-    for (key in mainObj){
-        objCopy[key] = mainObj[key];
-    }
-
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+const soldier = {
+    health: 400, 
+    armor: 150,
+    sayHello: function() {
+        console.log("Hello");
     }
 };
 
-// const newNumbers = copyFunc(numbers);
-// newNumbers.a = 10;
-// newNumbers.c.x = 99;
+const john = Object.create(soldier);
 
+// const john = {
+//     health: 100
+// };
 
-// console.log(numbers);
-// console.log(newNumbers);
+//john.__proto__ = soldier; //staroe
 
-const add = {
-    d: 17,
-    e: 20
-};
+//Object.setPrototypeOf(john, soldier); //novoe
 
-//console.log(Object.assign(numbers, add));
-
-const clone = Object.assign({}, add);
-clone.d = 99;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ["a","b","c"];
-
-const newArray = oldArray.slice();
-
-newArray[1] = "blablabal";
-
-console.log(newArray);
-console.log(oldArray);
-
-const video = ["youtube", "vimeo", "rutube"],
-      blogs = ["wordpress", "livejournal", "blogger"],
-      internet = [...video, ...blogs, "vk", "facebook"];
- 
-console.log(internet);
-
+//console.log(soldier);
+john.sayHello();
